60e1f4983a681b6e91561f0207795281d0228f1e
Oscar Ivan Gomez Zamudio <oigomezz@unal.edu.co>
2024-03-18 09:54:28 -0500

    Se agrega la seccion de Footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import "./App.css";
 import { Header } from "./components/Header";
 import { Products } from "./components/Products";
-import { products } from "./mocks/products.json";
+import { Footer } from './components/Footer.jsx'
+import { IS_DEVELOPMENT } from './config.js'
+
+import { products as initialProducts } from "./mocks/products.json";
 import { useState } from "react";
 
-function App() {
+function useFilters() {
   const [filters, setFilters] = useState({
     category: "all",
     minPrice: 0,
@@ -19,10 +22,20 @@ function App() {
     });
   };
 
+  return { filters, filterProducts, setFilters };
+}
+
+function App() {
+  const [products] = useState(initialProducts);
+  const { filters, filterProducts, setFilters } = useFilters();
+
+  const filteredProducts = filterProducts(products);
+
   return (
     <>
       <Header changefilters={setFilters} />
-      <Products products={filterProducts(products)} />;
+      <Products products={filteredProducts} />
+      {IS_DEVELOPMENT && <Footer filters={filters}/>}
     </>
   );
 }
