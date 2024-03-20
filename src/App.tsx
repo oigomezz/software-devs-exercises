b1241659aa44ca5ce4fcf4e65b20ba62da97e732
Oscar Ivan Gomez Zamudio <oigomezz@unal.edu.co>
2024-03-20 10:45:31 -0500

    Se agrega el useReducer

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,63 @@
+import { useReducer } from "react";
+import { Action, State } from "./type";
+
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 
+const initialState = {
+  fromLanguage: "auto",
+  toLanguage: "en",
+  fromText: "",
+  result: "",
+  loading: false,
+};
+
+function reducer(state: State, action: Action) {
+  const { type } = action;
+
+  if (type === "INTERCHANGE_LANGUAGES") {
+    return {
+      ...state,
+      fromLanguage: state.toLanguage,
+      toLanguage: state.fromLanguage,
+    };
+  }
+
+  if (type === "SET_FROM_LANGUAGE") {
+    return {
+      ...state,
+      fromLanguage: action.payload,
+    };
+  }
+
+  if (type === "SET_TO_LANGUAGE") {
+    return {
+      ...state,
+      toLanguage: action.payload,
+    };
+  }
+
+  if (type === "SET_FROM_TEXT") {
+    return {
+      ...state,
+      loading: true,
+      fromText: action.payload,
+    };
+  }
+
+  if (type === "SET_RESULT") {
+    return {
+      ...state,
+      loading: false,
+      result: action.payload,
+    };
+  }
+
+  return state;
+}
+
 function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
   return (
     <div className="App">
       <h1>Google Translate</h1>
