50d4dd4e37084a9f96f3b2515f9e9ec688c39b65
Oscar Ivan Gomez Zamudio <oigomezz@unal.edu.co>
2024-03-25 15:43:29 -0500

    Mostrar Resultados

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
 import "./App.css";
-import { Container, Stack, Typography } from "@mui/material";
+import { Container, Stack, Typography, useTheme } from "@mui/material";
+import useMediaQuery from "@mui/material/useMediaQuery";
+
 import { JavaScriptLogo } from "./JavaScriptLogo";
 import { Start } from "./page/Start";
 import { Game } from "./page/Game";
+import { Results } from "./page/Results";
 
 import { useQuestionsStore } from "./store/questions";
 import { useQuestionsData } from "./hooks/useQuestionsData";
@@ -10,6 +13,10 @@ import { useQuestionsData } from "./hooks/useQuestionsData";
 function App() {
   const questions = useQuestionsStore((state) => state.questions);
   const { unanswered } = useQuestionsData();
+
+  const theme = useTheme();
+  const medium = useMediaQuery(theme.breakpoints.up("md"));
+
   return (
     <main>
       <Container maxWidth="sm">
@@ -20,12 +27,13 @@ function App() {
           justifyContent="center"
         >
           <JavaScriptLogo />
-          <Typography variant="h2" component="h1">
+          <Typography variant={medium ? "h2" : "h5"} component="h1">
             JavaScript Quiz
           </Typography>
         </Stack>
         {questions.length === 0 && <Start />}
         {questions.length > 0 && unanswered > 0 && <Game />}
+        {questions.length > 0 && unanswered === 0 && <Results />}
       </Container>
     </main>
   );
--- src/store/questions.ts
+++ src/store/questions.ts
@@ -2,6 +2,7 @@ import { create } from "zustand";
 import { type Question } from "../types";
 
 import confetti from "canvas-confetti";
+import { persist } from "zustand/middleware";
 
 interface State {
   questions: Question[];
@@ -17,56 +18,68 @@ const API_URL = import.meta.env.PROD
   ? "https://midu-react-13.surge.sh/"
   : "http://localhost:5173/";
 
-export const useQuestionsStore = create<State>((set, get) => {
-  return {
-    questions: [],
-    currentQuestion: 0,
+export const useQuestionsStore = create<State>()(
+  persist(
+    (set, get) => {
+      return {
+        questions: [],
+        currentQuestion: 0,
 
-    fetchQuestions: async (limit: number) => {
-      const res = await fetch(`${API_URL}/data.json`);
-      const json = await res.json();
+        fetchQuestions: async (limit: number) => {
+          const res = await fetch(`${API_URL}/data.json`);
+          const json = await res.json();
 
-      const questions = json.sort(() => Math.random() - 0.5).slice(0, limit);
-      set({ questions });
-    },
+          const questions = json
+            .sort(() => Math.random() - 0.5)
+            .slice(0, limit);
+          set({ questions });
+        },
 
-    selectAnswer: (questionId: number, answerIndex: number) => {
-      const { questions } = get();
-      const newQuestions = structuredClone(questions);
-      const questionIndex = newQuestions.findIndex((q) => q.id === questionId);
-      const questionInfo = newQuestions[questionIndex];
-      const isCorrectUserAnswer = questionInfo.correctAnswer === answerIndex;
+        selectAnswer: (questionId: number, answerIndex: number) => {
+          const { questions } = get();
+          const newQuestions = structuredClone(questions);
+          const questionIndex = newQuestions.findIndex(
+            (q) => q.id === questionId
+          );
+          const questionInfo = newQuestions[questionIndex];
+          const isCorrectUserAnswer =
+            questionInfo.correctAnswer === answerIndex;
 
-      if (isCorrectUserAnswer) confetti();
+          if (isCorrectUserAnswer) confetti();
 
-      newQuestions[questionIndex] = {
-        ...questionInfo,
-        isCorrectUserAnswer,
-        userSelectedAnswer: answerIndex,
+          newQuestions[questionIndex] = {
+            ...questionInfo,
+            isCorrectUserAnswer,
+            userSelectedAnswer: answerIndex,
+          };
+
+          set({ questions: newQuestions });
+        },
+
+        goNextQuestion: () => {
+          const { currentQuestion, questions } = get();
+          const nextQuestion = currentQuestion + 1;
+
+          if (nextQuestion < questions.length) {
+            set({ currentQuestion: nextQuestion });
+          }
+        },
+
+        goPreviousQuestion: () => {
+          const { currentQuestion } = get();
+          const previousQuestion = currentQuestion - 1;
+
+          if (previousQuestion >= 0) {
+            set({ currentQuestion: previousQuestion });
+          }
+        },
+        reset: () => {
+          set({ currentQuestion: 0, questions: [] });
+        },
       };
-
-      set({ questions: newQuestions });
     },
-
-    goNextQuestion: () => {
-      const { currentQuestion, questions } = get();
-      const nextQuestion = currentQuestion + 1;
-
-      if (nextQuestion < questions.length) {
-        set({ currentQuestion: nextQuestion });
-      }
-    },
-
-    goPreviousQuestion: () => {
-      const { currentQuestion } = get();
-      const previousQuestion = currentQuestion - 1;
-
-      if (previousQuestion >= 0) {
-        set({ currentQuestion: previousQuestion });
-      }
-    },
-    reset: () => {
-      set({ currentQuestion: 0, questions: [] });
-    },
-  };
-});
+    {
+      name: "questions",
+    }
+  )
+);
