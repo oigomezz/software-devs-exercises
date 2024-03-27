6f4b66343a5c7d80da3de89fa9c71a4395b89b2f
Oscar Ivan Gomez Zamudio <oigomezz@unal.edu.co>
2024-03-27 12:47:25 -0500

    Operaciones CRUD basico

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
-const http = require("http");
+const express = require("express");
+const app = express();
+
+const PORT = 3001;
+app.use(express.json());
 
 let notes = [
   {
@@ -17,11 +21,76 @@ let notes = [
     important: true,
   },
 ];
-const app = http.createServer((request, response) => {
-  response.writeHead(200, { "Content-Type": "application/json" });
-  response.end(JSON.stringify(notes));
+
+const generateId = () => {
+  const maxId = notes.length > 0 ? Math.max(...notes.map((n) => n.id)) : 0;
+  return maxId + 1;
+};
+
+app.get("/", (request, response) => {
+  response.send("<h1>Hello World!</h1>");
 });
 
-const PORT = 3001;
-app.listen(PORT);
-console.log(`Server running on port ${PORT}`);
+app.get("/api/notes", (request, response) => {
+  response.json(notes);
+});
+
+app.get("/api/notes/:id", (request, response) => {
+  const id = Number(request.params.id);
+  const note = notes.find((note) => note.id === id);
+
+  if (note) response.json(note);
+  else response.status(404).end();
+});
+
+app.post("/api/notes", (request, response) => {
+  const body = request.body;
+
+  if (!body.content) {
+    return response.status(400).json({
+      error: "content missing",
+    });
+  }
+
+  const note = {
+    id: generateId(),
+    content: body.content,
+    important: Boolean(body.important) || false,
+  };
+
+  notes = notes.concat(note);
+
+  response.json(note);
+});
+
+app.put("/api/notes/:id", (request, response) => {
+  const { id } = request.params;
+  const note = request.body;
+
+  if (!note.content) {
+    return response.status(400).json({
+      error: "content missing",
+    });
+  }
+
+  const element = (note) => note.id === id;
+  const index = notes.findIndex(element);
+
+  notes[index] = {
+    ...note,
+    id: id,
+  };
+
+  response.json(notes[index]);
+});
+
+app.delete("/api/notes/:id", (request, response) => {
+  const id = Number(request.params.id);
+  notes = notes.filter((note) => note.id !== id);
+
+  response.status(204).end();
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
