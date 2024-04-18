aae9f884ae98fe94b86789b12a148a8163ee9bad
Oscar Ivan Gomez Zamudio <oigomezz@unal.edu.co>
2024-04-18 15:43:07 -0500

    Se agrega seguimiento de errores de Sentry, se actualiza dependencias y se modifican las rutas para incluir información del usuario

--- index.js
+++ index.js
@@ -1,61 +1,71 @@
 require('dotenv').config()
 require('./mongo')
 
+const Sentry = require('@sentry/node')
+const Tracing = require('@sentry/tracing')
 const express = require('express')
 const app = express()
 const cors = require('cors')
-
+const User = require('./models/User')
 const Note = require('./models/Note')
 
 const notFound = require('./middleware/notFound.js')
 const handleErrors = require('./middleware/handleErrors.js')
+const userExtractor = require('./middleware/userExtractor')
+
+const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
 
 app.use(cors())
 app.use(express.json())
 
+Sentry.init({
+  dsn: 'https://ac034ebd99274911a8234148642e044c@o537348.ingest.sentry.io/5655435',
+  integrations: [
+    // enable HTTP calls tracing
+    new Sentry.Integrations.Http({ tracing: true }),
+    // enable Express.js middleware tracing
+    new Tracing.Integrations.Express({ app })
+  ],
+
+  // We recommend adjusting this value in production, or using tracesSampler
+  // for finer control
+  tracesSampleRate: 1.0
+})
+
+// RequestHandler creates a separate execution context using domains, so that every
+// transaction/span/breadcrumb is attached to its own Hub instance
+app.use(Sentry.Handlers.requestHandler())
+// TracingHandler creates a trace for every incoming request
+app.use(Sentry.Handlers.tracingHandler())
+
 app.get('/', (request, response) => {
+  console.log(request.ip)
+  console.log(request.ips)
+  console.log(request.originalUrl)
   response.send('<h1>Hello World!</h1>')
 })
 
-app.get('/api/notes', (request, response) => {
-  Note.find({}).then(elements => {
-    response.json(elements)
+app.get('/api/notes', async (request, response) => {
+  const notes = await Note.find({}).populate('user', {
+    username: 1,
+    name: 1
   })
+  response.json(notes)
 })
 
 app.get('/api/notes/:id', (request, response, next) => {
   const { id } = request.params
+
   Note.findById(id)
     .then(note => {
       if (note) return response.json(note)
       response.status(404).end()
-    }).catch(err => next(err))
-})
-
-app.post('/api/notes', (request, response, next) => {
-  const {
-    content,
-    important = false
-  } = request.body
-
-  if (!content) {
-    return response.status(400).json({
-      error: 'required "content" field is missing'
     })
-  }
-
-  const note = new Note({
-    content,
-    date: new Date(),
-    important
-  })
-
-  note.save().then(savedNote => {
-    response.json(savedNote)
-  }).catch(err => next(err))
+    .catch(err => next(err))
 })
 
-app.put('/api/notes/:id', (request, response, next) => {
+app.put('/api/notes/:id', userExtractor, (request, response, next) => {
   const { id } = request.params
   const note = request.body
 
@@ -67,21 +77,61 @@ app.put('/api/notes/:id', (request, response, next) => {
   Note.findByIdAndUpdate(id, newNoteInfo, { new: true })
     .then(result => {
       response.json(result)
-    }).catch(err => next(err))
+    })
+    .catch(next)
 })
 
-app.delete('/api/notes/:id', (request, response, next) => {
+app.delete('/api/notes/:id', userExtractor, async (request, response, next) => {
   const { id } = request.params
-  const res = Note.findByIdAndDelete(id)
+  const res = await Note.findByIdAndDelete(id)
   if (res === null) return response.sendStatus(404)
 
   response.status(204).end()
 })
 
+app.post('/api/notes', userExtractor, async (request, response, next) => {
+  const {
+    content,
+    important = false
+  } = request.body
+
+  const { userId } = request
+  const user = await User.findById(userId)
+
+  if (!content) {
+    return response.status(400).json({
+      error: 'required "content" field is missing'
+    })
+  }
+
+  const newNote = new Note({
+    content,
+    date: new Date(),
+    important,
+    user: user._id
+  })
+
+  try {
+    const savedNote = await newNote.save()
+
+    user.notes = user.notes.concat(savedNote._id)
+    await user.save()
+
+    response.json(savedNote)
+  } catch (error) {
+    next(error)
+  }
+})
+
+app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
+
 app.use(notFound)
+
+app.use(Sentry.Handlers.errorHandler())
 app.use(handleErrors)
 
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 3001
 const server = app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
--- mongo.js
+++ mongo.js
@@ -6,11 +6,18 @@ const connectionString = NODE_ENV === 'test'
   ? MONGO_DB_URI_TEST
   : MONGO_DB_URI
 
+// comment for students puposes
 if (!connectionString) {
   console.error('Recuerda que tienes que tener un archivo .env con las variables de entorno definidas y el MONGO_DB_URI que servirá de connection string. En las clases usamos MongoDB Atlas pero puedes usar cualquier base de datos de MongoDB (local incluso).')
 }
 
-mongoose.connect(connectionString)
+// conexión a mongodb
+mongoose.connect(connectionString, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+  useCreateIndex: true
+})
   .then(() => {
     console.log('Database connected')
   }).catch(err => {
