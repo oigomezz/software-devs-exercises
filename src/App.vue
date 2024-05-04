c13fad913ed13a0a07debecd9976bf97fdc6b80c
oigomezz <oigomezz@unal.edu.co>
2024-05-04 11:53:49 -0500

    Update dependencies, add Home component and styling to App

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,27 @@
-<template></template>
+<template>
+  <Home />
+</template>
 
-<script></script>
+<script>
+import { Home } from "@/components/Home.vue";
+export default {
+  components: {
+    Home,
+  },
+};
+</script>
 
-<style></style>
+<style>
+html,
+body,
+.app {
+  min-height: 100vh;
+  margin: 0;
+  font-family: Arial, Helvetica, sans-serif;
+}
+
+* {
+  --brand-green: #04b500;
+  --brand-blue: #0689b0;
+}
+</style>
