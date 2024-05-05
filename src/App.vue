6ae4699046d8a9e0e9695b75bfa7d4c86e7770bd
oigomezz <oigomezz@unal.edu.co>
2024-05-04 20:33:18 -0500

    Remove ESLint config, add Suspense and async component for Home in App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,29 @@
 <template>
-  <Home />
+   <Suspense>
+    <template #default>
+      <Home />
+    </template>
+    <template #fallback>
+      <SplashScreen />
+    </template>
+  </Suspense>
 </template>
 
 <script>
-import { Home } from "@/components/Home.vue";
+import SplashScreen from "./components/SplashScreen.vue";
+import { defineAsyncComponent } from "vue";
+
 export default {
   components: {
-    Home,
+    SplashScreen,
+    Home: defineAsyncComponent(
+      () =>
+        new Promise((resolve) => {
+          setTimeout(() => {
+            resolve(import("./components/Home.vue"));
+          }, 2500);
+        })
+    ),
   },
 };
 </script>
