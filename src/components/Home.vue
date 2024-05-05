682e85b0e5a2ad4585de9d0ffd0e003f73649a8e
oigomezz <oigomezz@unal.edu.co>
2024-05-04 21:15:02 -0500

    "Add Resume component with local storage and formatting for movements"

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -4,7 +4,19 @@
       <Header></Header>
     </template>
     <template #resume>
-      <Resume></Resume>
+      <Resume
+        :total-label="'Ahorro total'"
+        :label="label"
+        :total-amount="totalAmount"
+        :amount="amount"
+      >
+        <template #graphic>
+          <Graphic :amounts="amounts" @select="select" />
+        </template>
+        <template #action>
+          <Action @create="create" />
+        </template>
+      </Resume>
     </template>
     <template #movements>
       <Movements></Movements>
@@ -23,6 +35,66 @@ export default {
     Header,
     Resume,
     Movements,
+  },
+  data() {
+    return {
+      label: null,
+      amount: null,
+      movements: [],
+    };
+  },
+  computed: {
+    amounts() {
+      const lastDays = this.movements
+        .filter((m) => {
+          const today = new Date();
+          const oldDate = today.setDate(today.getDate() - 30);
+
+          return m.time > oldDate;
+        })
+        .map((m) => m.amount);
+
+      return lastDays.map((m, i) => {
+        const lastMovements = lastDays.slice(0, i + 1);
+
+        return lastMovements.reduce((suma, movement) => {
+          return suma + movement;
+        }, 0);
+      });
+    },
+    totalAmount() {
+      return this.movements.reduce((suma, m) => {
+        return suma + m.amount;
+      }, 0);
+    },
+  },
+  mounted() {
+    const movements = JSON.parse(localStorage.getItem("movements"));
+    console.log(movements);
+
+    if (Array.isArray(movements)) {
+      this.movements = movements.map((m) => {
+        return { ...m, time: new Date(m.time) };
+      });
+    }
+  },
+  methods: {
+    create(movement) {
+      this.movements.push(movement);
+      this.save();
+    },
+    remove(id) {
+      const index = this.movements.findIndex((m) => m.id === id);
+      this.movements.splice(index, 1);
+      this.save();
+    },
+    save() {
+      localStorage.setItem("movements", JSON.stringify(this.movements));
+    },
+    select(el) {
+      console.log(el);
+      this.amount = el;
+    },
   },
 };
 </script>
