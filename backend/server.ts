8fedb5fc3b0403c270d33c055c7305885f56842e
Oscar Ivan Gomez Zamudio <oigomezz@unal.edu.co>
2024-05-06 21:20:11 -0500

    Added Multer and convert-csv-to-json packages to handle CSV file upload and parsing
    This commit adds the necessary dependencies, middleware, and code to accept a CSV file upload

--- backend/server.ts
+++ backend/server.ts
@@ -1,22 +1,47 @@
 import express from "express";
 import cors from "cors";
+import multer from "multer";
+import csvToJson from "convert-csv-to-json";
+import { log } from "console";
 
 const app = express();
 const port = process.env.PORT ?? 3000;
 
+const storage = multer.memoryStorage();
+const upload = multer({ storage });
+
+let userData: Array<Record<string, string>> = [];
+
+app.use(express.json());
+
 // Enable CORS
 app.use(cors());
 
-app.post("/api/files", async (req, res) => {
+app.post("/api/files", upload.single("file"), async (req, res) => {
   // 1. Extract file from request
+  const { file } = req;
   // 2. Validate that we have file
+  if (!file) return res.status(500).json({ message: "File is requered" });
   // 3. Validate that mimetype (csv)
-  // 4. Transform the file (buffer) to a string
-  // 5. Transform string to CSV
+  if (file.mimetype !== "text/csv")
+    return res.status(500).json({ message: "File must be CSV" });
+
+  let json: Array<Record<string, string>> = [];
+
+  try {
+    // 4. Transform the file (buffer) to a string
+    const csv = Buffer.from(file.buffer).toString("utf-8");
+    // console.log(csv);
+    // 5. Transform string to JSON
+    json = csvToJson.csvStringToJson(csv);
+  } catch (error) {
+    return res.status(500).json({ message: "Error parsing the file" });
+  }
   // 6. Save the JSON to db (or memory)
+  userData = json;
   // 7. Return 200 with the message and the JSON
   return res.status(200).json({
-    data: [],
+    data: json,
     message: "El archivo se cargó correctamente",
   });
 });
