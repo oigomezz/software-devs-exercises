6a22960465cecad353010e770fbc43bce65f486e
oigomezz <oigomezz@unal.edu.co>
2024-09-05 17:13:49 -0500

    Added RegisterPage to App.jsx route

--- airbnb-clone/client/src/App.jsx
+++ airbnb-clone/client/src/App.jsx
@@ -3,6 +3,7 @@ import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import IndexPage from "./pages/IndexPage.jsx";
 import LoginPage from "./pages/LoginPage.jsx";
+import RegisterPage from "./pages/RegisterPage.jsx";
 import Layout from "./Layout.jsx";
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
       <Route path="/" element={<Layout />}>
         <Route index element={<IndexPage />} />
         <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
       </Route>
     </Routes>
   );
