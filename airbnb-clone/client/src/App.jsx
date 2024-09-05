6676a80d636499a168e5d88ffa0dc3cfbbce806e
oigomezz <oigomezz@unal.edu.co>
2024-09-05 17:08:35 -0500

    Updated App.jsx to add LoginPage and Layout components, and modified index.css to add styles for form inputs and buttons.

--- airbnb-clone/client/src/App.jsx
+++ airbnb-clone/client/src/App.jsx
@@ -2,11 +2,16 @@ import "./App.css";
 
 import { Route, Routes } from "react-router-dom";
 import IndexPage from "./pages/IndexPage.jsx";
+import LoginPage from "./pages/LoginPage.jsx";
+import Layout from "./Layout.jsx";
 
 function App() {
   return (
     <Routes>
-      <Route index element={<IndexPage />} />
+      <Route path="/" element={<Layout />}>
+        <Route index element={<IndexPage />} />
+        <Route path="/login" element={<LoginPage />} />
+      </Route>
     </Routes>
   );
 }
