23e101254c920c2395fd4d8fe6fb352fcf6b9c40
oigomezz <oigomezz@unal.edu.co>
2024-09-05 17:48:17 -0500

    Init API

---
+++ airbnb-clone/api/index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const cors = require("cors");
+
+require("dotenv").config();
+const app = express();
+
+app.use(express.json());
+app.use(
+  cors({
+    credentials: true,
+    origin: "http://127.0.0.1:5173",
+  })
+);
+
+const PORT = process.env.PORT || 3001;
+const server = app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
+module.exports = { app, server };
