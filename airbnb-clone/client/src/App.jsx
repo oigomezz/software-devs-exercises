f6b1d0649e715afce87fbeffdecf887f3fb0a2de
oigomezz <oigomezz@unal.edu.co>
2024-09-09 12:29:08 -0500

    Added booking functionality

--- airbnb-clone/client/src/App.jsx
+++ airbnb-clone/client/src/App.jsx
@@ -30,6 +30,8 @@ function App() {
           <Route path="/account/places/new" element={<PlacesFormPage />} />
           <Route path="/account/places/:id" element={<PlacesFormPage />} />
           <Route path="/place/:id" element={<PlacePage />} />
+          <Route path="/account/bookings" element={<BookingsPage />} />
+          <Route path="/account/bookings/:id" element={<BookingPage />} />
         </Route>
       </Routes>
     </UserContextProvider>
