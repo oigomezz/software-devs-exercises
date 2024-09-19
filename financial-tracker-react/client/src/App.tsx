38187fde47c8cc48e9e7b6f4d49c0690b19619d9
oigomezz <oigomezz@unal.edu.co>
2024-09-19 17:21:21 -0500

    Updated .gitignore, package.json, and added/modified CSS and React components in client/src

--- financial-tracker-react/client/src/App.tsx
+++ financial-tracker-react/client/src/App.tsx
@@ -1,20 +1,22 @@
-import {
-  SignedIn,
-  SignedOut,
-  SignInButton,
-  UserButton,
-} from "@clerk/clerk-react";
 import "./App.css";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { SignedIn, UserButton } from "@clerk/clerk-react";
 
 export default function App() {
   return (
-    <header>
-      <SignedOut>
-        <SignInButton />
-      </SignedOut>
-      <SignedIn>
-        <UserButton />
-      </SignedIn>
-    </header>
+    <Router>
+      <div className="app-container">
+        <div className="navbar">
+          <Link to="/"> Dashboard</Link>
+          <SignedIn>
+            <UserButton />
+          </SignedIn>
+        </div>
+        <Routes>
+          <Route path="/" element={<h1> Dashboard </h1>} />
+          <Route path="/auth" element={<h1> Sing In </h1>} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
